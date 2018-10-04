85ac06d32723fe9568718e55543ae3508524f3f3
Nate Miller <natekmiller96@gmail.com>
2018-10-04 20:25:08 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 function produceDrivingRange(maxBlocks){
-  return function(blocks){
-    if (blocks > maxBlocks){
+  return function(blocks1, blocks2){
+    if (Math.abs(blocks1.slice('th')[0] - blocks2.slice('th')[0]) > maxBlocks){
       return false;
     }else {
       return true;
