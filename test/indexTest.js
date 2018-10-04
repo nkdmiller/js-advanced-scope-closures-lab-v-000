00d038bcc1ba248357408ed38bf89e4fd38b984a
Nate Miller <natekmiller96@gmail.com>
2018-10-04 20:34:39 +0000

    automatically backed up by learn

--- test/indexTest.js
+++ test/indexTest.js
@@ -8,7 +8,7 @@ describe('closures', function() {
 
     it('takes an argument of blockRange which is then used to calculate if something is within range', function(){
       let eightBlockRange = produceDrivingRange(8)
-      expect(eightBlockRange('10th', '19th')).to.equal('2 blocks out of range')
+      expect(eightBlockRange('10th', '20th')).to.equal('2 blocks out of range')
       expect(eightBlockRange('10th', '14th')).to.equal('within range by 4')
     })
 
