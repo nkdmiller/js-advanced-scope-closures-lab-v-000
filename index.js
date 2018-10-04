2207c0986c2628cb74a6764d31095edfb9f16230
Nate Miller <natekmiller96@gmail.com>
2018-10-04 20:37:08 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ function produceDrivingRange(maxBlocks){
   return function(blocks1, blocks2){
     distance = parseInt(blocks2.split('t')[0]) - parseInt(blocks1.split('t')[0]);
     if ( distance > maxBlocks){
-      return `out of range`;
+      return `${maxBlocks - distance} out of range`;
     }else {
       return `within range by ${distance}`;
       }
