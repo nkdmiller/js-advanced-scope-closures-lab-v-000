66ecda40dd8dc6ce9cdf25f0f72368085f69a2f5
Nate Miller <natekmiller96@gmail.com>
2018-10-04 20:45:19 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,27 @@
+function produceDrivingRange(maxBlocks){
+  let distance = 0;
+  return function(blocks1, blocks2){
+    distance = parseInt(blocks2.split('t')[0]) - parseInt(blocks1.split('t')[0]);
+    if ( distance > maxBlocks){
+      return `${Math.abs(maxBlocks - distance)} blocks out of range`;
+    }else {
+      return `within range by ${distance}`;
+      }
+    };
+  }
+function produceTipCalculator(rate){
+  return function(total){
+    return total * rate;
+  };
+}
+
+function createDriver(){
+  let driverId = 0;
+  
+  return class {
+    constructor(name){
+      this.name = name;
+      this.id = ++driverId;
+    }
+  };
+}
